135215a544ffb9cf671ea793ae77bff3946ffc47
tetiana_hushnovska <thushnyovska@gmail.com>
2024-11-21 17:10:13 +0000

    add js file

--- js/task-3.js
+++ js/task-3.js
@@ -6,4 +6,4 @@ function getElementWidth(content, padding, border) {
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth("200px", "432px", "13.13px")); // 200
